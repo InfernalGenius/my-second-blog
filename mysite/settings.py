5172bfaa9a1a79689eee75d71d3545698d79578f
stefanie <koenen.steffi@gmail.com>
2022-11-25 22:07:13 +0100

    Path fix nummero 2

--- mysite/settings.py
+++ mysite/settings.py
@@ -13,7 +13,7 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production
